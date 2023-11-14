e8cda88a0fb1bb139ff6b80e210ad3e822193c85
ChoiHs <chs991209@gmail.com>
2023-11-14 10:11:23 +0900

    ADD: return값 배열 알고리즘 변경(수정)

--- src/services/categoryService.js
+++ src/services/categoryService.js
@@ -2,16 +2,19 @@ const categoryDao = require('../models/categoryDao');
 const error = require('../utils/error');
 
 const getCategory = async (type) => {
-  let result = [];
+  let categoryIds = [1, 2, 4];
+  let result = []
   if (type === '지출') {
-    for (let categoryId in [1, 2, 4]) {
-      result.push(await categoryDao.getCategory(categoryId));
-      result[result.length - 1]['type'] = type;
+    for (let i = 0; i < 3; i++) {
+      let category = await categoryDao.getCategory(categoryIds[i]);
+      result[i] = await category[0];
+      result[i].type = type;
     }
   }
   else if (type === '수입') {
-    result.push(await categoryDao.getCategory(3));
-    result[0]['type'] = type;
+    let category = await categoryDao.getCategory(3)
+    result[0] = await category[0]
+    result[0].type = type;
   }
   return result;
 }
