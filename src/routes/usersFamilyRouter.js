6241bc6516cf502be311633a7e0ec2de6b08dfbd
ChoiHs <chs991209@gmail.com>
2023-11-15 19:55:17 +0900

    ADD : 가족이름목록조회 더블 quotes 수정

--- src/routes/usersFamilyRouter.js
+++ src/routes/usersFamilyRouter.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const usersFamilyController = require("../controllers/usersFamilyController");
+const usersFamilyController = require('../controllers/usersFamilyController');
 
 router.get('/user', usersFamilyController.getUsersFamilyByUsersId);
 
