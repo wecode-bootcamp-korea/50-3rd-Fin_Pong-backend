3cb61ad82684b8a2fb138807851fdc228fa8e546
wonkyujeong <jeongwonkyu81@gmail.com>
2023-11-16 20:23:41 +0900

    ADD : login 기능 완성 및 error.js 추가

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userRouter = require('../routes/userRouter');
-const familyRouter = require('../routes/familyRouter');
 
 router.use('/users', userRouter.router);
-router.use('/family', familyRouter.router);
 
 module.exports = router;
