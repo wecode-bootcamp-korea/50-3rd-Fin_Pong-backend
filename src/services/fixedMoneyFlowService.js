3399afe3b423341e72443199c9a999462eb48bab
ChoiHs <chs991209@gmail.com>
2023-11-20 15:48:10 +0900

    ADD: 예산조회등록변경 fixedMoneyFlowService.js line4 주석 추가

--- src/services/fixedMoneyFlowService.js
+++ src/services/fixedMoneyFlowService.js
@@ -1,7 +1,7 @@
 const fixedMoneyFlowDao = require('../models/fixedMoneyFlowDao');
 const error = require('../utils/error');
 
-const getUsedFixedMoneyFlowsByYearMonthAndGetAmount = async (userId, year, month) => { // 월 별
+const getUsedFixedMoneyFlowsByYearMonthAndGetAmount = async (userId, year, month) => { // 월 별 고정 지출 사용량을 합산합니다.
   let typeId = 2;
   const flows = await fixedMoneyFlowDao.getFixedMoneyFlowsByYearMonth(userId, typeId, year, month);
   const mapped = await Promise.all(flows.map( async (flow) => ({
