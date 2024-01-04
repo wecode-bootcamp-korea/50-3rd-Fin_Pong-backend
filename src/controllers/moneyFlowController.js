51faab53d44b20af7619728085e8f6c55d689601
ChoiHs <chs991209@gmail.com>
2024-01-04 12:04:01 +0900

    MODIFY, SETTING: api/개인수입지출조회 주석 수정, package.json 추가

--- src/controllers/moneyFlowController.js
+++ src/controllers/moneyFlowController.js
@@ -26,7 +26,7 @@ const getMoneyFlowsByCondition = async (req, res) => {
     if (!year && (month || date)) { // 연도가 없고 월, 날짜 조건이 있는 경우  => 연도를 입력해 주세요
       error.throwErr(400, 'KEY_ERROR_SELECT_A_YEAR');
     }
-    if (!familyId) {
+    else if (!familyId) { // 가족에 가입하지 않은 사용자가 본인의 수입/지출 내역을 조회합니다.
       if (!year && !month && !date) { // 연도, 월, 날짜의 조건이 없는 경우 => 해당 유저의 수입/지출내역을 모두 찾습니다.
         const moneyFlows = await moneyFlowService.getMoneyFlowsByUserId(userId);
         return res.status(200).json({message: 'GET_SUCCESS', flows: moneyFlows});
@@ -46,46 +46,46 @@ const getMoneyFlowsByCondition = async (req, res) => {
       const moneyFlows = await moneyFlowService.getMoneyFlowsByUserIdByYearMonthDate(userId, year, month, date); // 해당 유저의 해당 연, 월, 날짜의 수입/지출 내역을 찾습니다.
       return res.status(200).json({message: 'GET_SUCCESS', flows: moneyFlows});
     }
-    else if (!userName) {
+    else if (!userName) { // 가족에 가입된 사용자가 가족 구성원 전체의 수입/지출 내역을 조회합니다.
       const familyUsersIds = await usersFamilyService.getFamilyUsersIds(familyId);
-      if (!year && !month && !date) { // 연도, 월, 날짜의 조건이 없는 경우 => 해당 유저의 수입/지출내역을 모두 찾습니다.
+      if (!year && !month && !date) { // 연도, 월, 날짜의 조건이 없는 경우 => 가족 구성원 전체가 등록한 수입/지출 내역을 모두 찾습니다.
         const moneyFlows = await moneyFlowService.getMoneyFlowsByFamilyUserId(familyUsersIds);
         return res.status(200).json({message: 'GET_SUCCESS', flows: moneyFlows});
       }
-      else if (year && !month && !date) { // 연도 조건만 있고, 월, 날짜 조건은 없는 경우 => 가족 구성원의 해당 연도의 모든 수입/지출 내역을 찾습니다.
+      else if (year && !month && !date) { // 연도 조건만 있고, 월, 날짜 조건은 없는 경우 => 해당 연도에 발생한 가족 구성원 전체의 모든 수입/지출 내역을 찾습니다.
         const moneyFlows = await moneyFlowService.getMoneyFlowsByFamilyUserIdByYear(familyUsersIds, year);
         return res.status(200).json({message: 'GET_SUCCESS', flows: moneyFlows});
       }
       else if (year && month && !date) { // 연도, 월 조건만 있고, 날짜 조건은 없는 경우
-        const moneyFlows = await moneyFlowService.getMoneyFlowsByFamilyUserIdByYearMonth(familyUsersIds, year, month); // 해당 유저의 해당 연, 월의 수입/지출 내역을 찾습니다.
+        const moneyFlows = await moneyFlowService.getMoneyFlowsByFamilyUserIdByYearMonth(familyUsersIds, year, month); // 해당 연, 월에 발생한 가족 구성원 전체의 수입/지출 내역을 찾습니다.
         return res.status(200).json({message: 'GET_SUCCESS', flows: moneyFlows});
       }
       else if (year && !month && date) { // 월 조건만 없고, 연, 날짜 조건만 있는 경우 (ex.2023년의 매달 1일에 무엇을 쓰고 벌었는 지 알려줘)
-        const moneyFlows = await moneyFlowService.getMoneyFlowsByFamilyUserIdByYearDate(familyUsersIds, year, date); // 해당 유저의 해당 연도의 해당 날짜의 수입/지출 내역들을 찾습니다.
+        const moneyFlows = await moneyFlowService.getMoneyFlowsByFamilyUserIdByYearDate(familyUsersIds, year, date); // 해당 연도, 해당 날짜에 발생한 가족 구성원 전체의 수입/지출 내역들을 찾습니다.
         return res.status(200).json({message: 'GET_SUCCESS', flows: moneyFlows});
       }
-      const moneyFlows = await moneyFlowService.getMoneyFlowsByFamilyUserIdByYearMonthDate(familyUsersIds, year, month, date); // 가족 구성원의 해당 연, 월, 날짜의 수입/지출 내역을 찾습니다.
+      const moneyFlows = await moneyFlowService.getMoneyFlowsByFamilyUserIdByYearMonthDate(familyUsersIds, year, month, date); // 해당 연, 월, 날짜에 발생한 가족 구성원 전체의 수입/지출 내역을 찾습니다.
       return res.status(200).json({message: 'GET_SUCCESS', flows: moneyFlows});
     }
-    else if (userName) { // 특정 유저의 수입/지출을 찾으려는 경우
+    else if (userName) { // 특정 가족 구성원의 수입/지출을 찾으려는 경우
       const userId = await usersFamilyService.getAuthenticUserId(familyId, userName); // familyId 정보와 유저의 이름으로 유저 id를 찾습니다.
       if (!year && !month && !date) { // 연도, 월, 날짜의 조건이 없는 경우 => 해당 유저의 수입/지출내역을 모두 찾습니다.
         const moneyFlows = await moneyFlowService.getMoneyFlowsByUserId(userId);
         return res.status(200).json({message: 'GET_SUCCESS', flows: moneyFlows});
       }
-      else if (year && !month && !date) { // 연도 조건만 있고, 월, 날짜 조건은 없는 경우 => 해당 유저의 해당 연도의 모든 수입/지출 내역을 찾습니다.
+      else if (year && !month && !date) { // 연도 조건만 있고, 월, 날짜 조건은 없는 경우 => 조회 대상 가족 구성원의 해당 연도의 모든 수입/지출 내역을 찾습니다.
         const moneyFlows = await moneyFlowService.getMoneyFlowsByUserIdByYear(userId, year);
         return res.status(200).json({message: 'GET_SUCCESS', flows: moneyFlows});
       }
       else if (year && month && !date) { // 연도, 월 조건만 있고, 날짜 조건은 없는 경우
-        const moneyFlows = await moneyFlowService.getMoneyFlowsByUserIdByYearMonth(userId, year, month); // 해당 유저의 해당 연, 월의 수입/지출 내역을 찾습니다.
+        const moneyFlows = await moneyFlowService.getMoneyFlowsByUserIdByYearMonth(userId, year, month); // 조회 대상 가족 구성원의 해당 연, 월의 수입/지출 내역을 찾습니다.
         return res.status(200).json({message: 'GET_SUCCESS', flows: moneyFlows});
       }
-      else if (year && !month && date) { // 월 조건만 없고, 연, 날짜 조건만 있는 경우 (ex.2023년의 매달 1일에 무엇을 쓰고 벌었는 지 알려줘)
-        const moneyFlows = await moneyFlowService.getMoneyFlowsByUserIdByYearDate(userId, year, date); // 해당 유저의 해당 연도의 해당 날짜의 수입/지출 내역들을 찾습니다.
+      else if (year && !month && date) { // 월 조건만 없고, 연, 날짜 조건만 있는 경우 (ex.2023년의 매 1일에 등록된 모든 수입/지출 내역을 알려줘)
+        const moneyFlows = await moneyFlowService.getMoneyFlowsByUserIdByYearDate(userId, year, date); // 조회 대상 가족 구성원의 해당 연도의 해당 날짜의 수입/지출 내역들을 찾습니다.
         return res.status(200).json({message: 'GET_SUCCESS', flows: moneyFlows});
       }
-      const moneyFlows = await moneyFlowService.getMoneyFlowsByUserIdByYearMonthDate(userId, year, month, date); // 해당 유저의 해당 연, 월, 날짜의 수입/지출 내역을 찾습니다.
+      const moneyFlows = await moneyFlowService.getMoneyFlowsByUserIdByYearMonthDate(userId, year, month, date); // 조회 대상 가족 구성원의 해당 연, 월, 날짜의 수입/지출 내역을 찾습니다.
       return res.status(200).json({message: 'GET_SUCCESS', flows: moneyFlows});
     }
   } catch (err) {
